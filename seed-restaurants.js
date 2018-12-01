214d6b99b1903ca4c8345c2517fcdd22abb5c31b
Robert Larsen <robert@robert-larsen.net>
2018-12-01 14:54:30 +0200

    Using parameter store.

--- seed-restaurants.js
+++ seed-restaurants.js
@@ -49,7 +49,12 @@ let restaurants = [
 ];
 
 const getTableName = async () => {
-  return `restaurants-${STAGE}-robert`
+  console.log('getting table name...')
+  const req = {
+    Name: `/workshop-robert/${STAGE}/table_name`
+  }
+  const ssmResp = await ssm.getParameter(req).promise()
+  return ssmResp.Parameter.Value
 }
 
 const run = async () => {
